8f671b30a360f1b5a566166d62a1763933d907b9
Cole Holmes <mitchellcholmes@gmail.com>
2019-09-27 13:24:39 -0500

    Updates

---
+++ precache-manifest.dd5b85fb097c541d8b56351a9b23ee6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d810734fb41f048ab93dfc78021472",
+    "url": "/p4assementTake2/index.html"
+  },
+  {
+    "revision": "b1005c71b747cc2a0bcc",
+    "url": "/p4assementTake2/static/css/main.abddb58e.chunk.css"
+  },
+  {
+    "revision": "0cc549c3b5dc81b00661",
+    "url": "/p4assementTake2/static/js/2.19100ec5.chunk.js"
+  },
+  {
+    "revision": "b1005c71b747cc2a0bcc",
+    "url": "/p4assementTake2/static/js/main.1bc96d0b.chunk.js"
+  },
+  {
+    "revision": "8c18f995640380c0e5be",
+    "url": "/p4assementTake2/static/js/runtime-main.7d2ec272.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/p4assementTake2/precache-manifest.dd5b85fb097c541d8b56351a9b23ee6e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/p4assementTake2/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
